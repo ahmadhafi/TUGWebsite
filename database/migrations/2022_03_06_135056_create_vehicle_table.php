61320ba4a936f714fddca0c15f8565c91b9a30e1
Ahmad Bisyrul Hafi <72262037+ahmadhafi@users.noreply.github.com>
2022-03-13 22:02:04 +0700

    Add files via upload

---
+++ database/migrations/2022_03_06_135056_create_vehicle_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateVehicleTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('vehicle', function (Blueprint $table) {
+            $table->id('id_kendaraan');
+            $table->string('jenis_kendaraan');
+            $table->string('nomor_kendaraan');
+            $table->string('unit_kerja');
+            $table->string('PIC');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('vehicle');
+    }
+};
