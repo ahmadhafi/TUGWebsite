780f9981e410b9d6c20c48c81cad18b90497903d
Ahmad Bisyrul Hafi <bisyrulhafiahmad@gmail.com>
2022-03-14 22:50:44 +0700

    welcome page

---
+++ resources/views/home.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.main')
+@section('container')
+
+<div class="container-baru">
+<!-- jumbotron -->
+    <section class="jumbotron home text-center">
+        <img src="img/logo.png" class="img-thumbnail" alt="logo">
+        <h1 class="display-6">Stable, Strong, Safe</h1>
+    </section>
+        <section class="aplikasi-utama-tug-gmf">
+            <div class="container-fluid arti-gmf">
+                <div class="row d-flex justify-content-center">
+                    <h1>PT Garuda Maintenance Facility</h1>
+                </div>
+                <div class="row d-flex justify-content-center">
+                    <hr>
+                </div>
+                <div class="row d-flex justify-content-center">
+                    <p>Perusahaan BUMN yang bergerak dalam bidang perbaikan pesawat</p>
+                </div>
+            </div>
+        </section>
+</div>
